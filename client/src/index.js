17a4e6ff9b94e0300e0cbeb526ba9156071c173f
faizin <nasrunfaizinkholiq@gmail.com>
2024-09-10 01:06:03 +0700

    setup react for client

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RoutesConfig from './routes';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <RoutesConfig />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ client/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h2>Welcome to Workaszn</h2>
+    <p>Your project management dashboard.</p>
+  </div>
+);
+
+export default Home;
